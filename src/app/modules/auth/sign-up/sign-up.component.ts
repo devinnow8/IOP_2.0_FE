e5fcfdadc1d02d40152c960e892ebcb95d02add2
Ashutosh Sharma <ashutoshsharma@ninjacart.com>
2023-04-26 11:04:47 +0530

    feat: sign up API integration

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '@app/core/servcies/auth.service';
+import { Router} from '@angular/router';
+import { UserService } from '@app/core/servcies/user.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  signUpForm!: FormGroup;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private userService: UserService
+  ) {
+    this.initForm();
+  }
 
+  initForm() {
+    this.signUpForm = new FormGroup({
+      firstname: new FormControl('', { nonNullable: true }),
+      lastname: new FormControl('', { nonNullable: true }),
+      email: new FormControl('', { nonNullable: true }),
+      phone_number: new FormControl('', { nonNullable: true }),
+      password: new FormControl('', { nonNullable: true }),
+    });
+  }
+
+  signUp() {
+    const formData = this.signUpForm?.value;
+    console.log("formData", formData)
+    this.authService.signUp(formData).subscribe({next: (res) => {
+      this.userService.storeUser(res);
+      this.router.navigate(['/dashboard'], {replaceUrl:true});
+    }})
+    
+  }
 }
